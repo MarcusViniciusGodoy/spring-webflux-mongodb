05631df3df931107e31ee3e85beae06671643688
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2026-03-07 16:29:48 -0300

    Get all users, users by id

--- spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -1,16 +1,8 @@
 package com.devsuperior.workshopmongo.services;
 
-import java.time.Instant;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.devsuperior.workshopmongo.dto.PostDTO;
-import com.devsuperior.workshopmongo.entities.Post;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
-import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
 
 @Service
 public class PostService {
