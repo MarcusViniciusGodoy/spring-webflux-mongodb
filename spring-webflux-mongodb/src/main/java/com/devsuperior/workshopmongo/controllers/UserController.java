6f015c8b2605b0844d6d4de869aa681217561bcf
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2026-03-14 13:44:00 -0300

    Delete user

--- spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -46,4 +46,9 @@ public class UserController {
 	public Mono<ResponseEntity<UserDTO>> update(@PathVariable String id, @RequestBody UserDTO dto) {
 		return service.update(id, dto).map(userUpdated -> ResponseEntity.ok().body(userUpdated));
 	}
+
+	@PutMapping(value = "/{id}")
+	public Mono<ResponseEntity<Void>> delete(@PathVariable String id) {
+		return service.delete(id).then(Mono.just(ResponseEntity.noContent().<Void>build()));
+	}
 }
