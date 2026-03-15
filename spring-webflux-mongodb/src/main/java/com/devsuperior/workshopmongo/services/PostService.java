977786fd2b9535bbac8037b5c365a18e7e0bb568
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2026-03-15 14:46:03 -0300

    Post find by title

--- spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -7,6 +7,7 @@ import com.devsuperior.workshopmongo.dto.PostDTO;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
 import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -20,12 +21,12 @@ public class PostService {
 				.map(existingPost -> new PostDTO(existingPost))
 				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
 	}
-	/*
-	public List<PostDTO> findByTitle(String text) {
-		List<PostDTO> result = repository.searchTitle(text).stream().map(x -> new PostDTO(x)).toList();
-		return result;
-	}
 	
+	public Flux<PostDTO> findByTitle(String text) {
+		return repository.searchTitle(text)
+				.map(postFound -> new PostDTO(postFound));
+	}
+	/*
 	public List<PostDTO> fullSearch(String text, Instant minDate, Instant maxDate) {
 		maxDate = maxDate.plusSeconds(86400); // 24 * 60 * 60
 		List<PostDTO> result = repository.fullSearch(text, minDate, maxDate).stream().map(x -> new PostDTO(x)).toList();
